872fb3241ee43609f4d2c71bbf2141f565c1e5c0
msbactrungnam <phungbacong2002@gmail.com>
2023-03-24 18:39:10 +0700

    delete

--- javascript-exercise/exercise-1/exercise-1.js
+++
@@ -1,9 +0,0 @@
-function repeatString(string, num) {
-  if (!string || !num) return;
-  var s = "";
-  for(var i = 0; i<num;i++){
-    s+=string;
-  }
-  return console.log(s);;
-}
-repeatString("hey",3)
--- javascript-exercise/exercise-2/exercise-2.js
+++
@@ -1,8 +0,0 @@
-function reverseString(string) {
-  if (!string) return;
-  var arr = string.split("");
-  var arr1 = arr.reverse();
-  var s = arr1.join("");
-  return console.log(s);
-}
-reverseString("hello there");
